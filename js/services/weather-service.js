60d57b84f4f5fd2c6a6735ce8c5e5d908e5ac62f
Lian Skopis <lian.skopis@gmail.com>
2021-02-18 03:25:57 +0200

    fghfggh

--- js/services/weather-service.js
+++ js/services/weather-service.js
@@ -1,29 +1,30 @@
 export const weatherService = {
-    onGetWeather
+    // onGetWeather,
+    renderWeatherWidget
 }
 
-let lat = 32.073582;
-let lon = 34.788052;
+// let lat = 32.073582;
+// let lon = 34.788052;
 const W_KEY = '6c53975efc59be8a72b725abf375b7d4'
-const WEATHER_BASE_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${W_KEY}`
+// const WEATHER_BASE_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${W_KEY}`
 
 
 
-function getWeather(cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-            let info = JSON.parse(xhr.responseText);
-            cb(info)
-        }
-    }
-    xhr.open("GET", WEATHER_BASE_URL, true);
-    xhr.send();
-}
+// function getWeather(cb) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//             let info = JSON.parse(xhr.responseText);
+//             cb(info)
+//         }
+//     }
+//     xhr.open("GET", WEATHER_BASE_URL, true);
+//     xhr.send();
+// }
 
-function onGetWeather() {
-    getWeather(renderWeather)
-}
+// function onGetWeather() {
+//     getWeather(renderWeather)
+// }
 
 function renderWeather(weatherObj) {
     const weatherInfo = weatherObj
@@ -33,48 +34,21 @@ function renderWeather(weatherObj) {
 }
 
 
-
-function getCities(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-
-getCities("js/services/city.list.json", function(text){
-    const cities = JSON.parse(text);
-    getCityId(cities)
-});
-
-
-function getCityId(cities) {
-    let cityName = 'Tokyo'
-    cities.find(city => {
-        if (city.name === cityName) console.log(city.id);
-    })
-}
-
-renderWeatherWidget('1850147')
-
-function renderWeatherWidget(cityId) {
+function renderWeatherWidget(cityIDX) {
     window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
     window.myWidgetParam.push({
         id: 12,
-        cityid: cityId,
+        cityid:cityIDX,
         appid: '6c53975efc59be8a72b725abf375b7d4',
         units: 'metric',
-        containerid: 'openweathermap-widget-12',  });
-        (function() {
-            var script = document.createElement('script');
-            script.async = true;script.charset = "utf-8";
-            script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(script, s);})()
+        containerid: 'openweathermap-widget-12',
+    });
+    (function () {
+        var script = document.createElement('script');
+        script.async = true; script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+    })()
 
-        }
+}
